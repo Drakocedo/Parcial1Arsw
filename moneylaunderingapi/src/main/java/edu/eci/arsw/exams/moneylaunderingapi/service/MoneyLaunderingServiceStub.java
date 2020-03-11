4b0a30f74b62137316b5b9b0ff99f84e15e02e10
unknown <david.caycedo@labinfo.is.escuelaing.edu.co>
2020-03-11 09:57:11 -0500

    avance

--- moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/service/MoneyLaunderingServiceStub.java
+++ moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/service/MoneyLaunderingServiceStub.java
@@ -12,9 +12,30 @@ import org.springframework.stereotype.Component;
 
 @Component("MoneyLaunderingService")
 public class MoneyLaunderingServiceStub implements MoneyLaunderingService {
-	
-	
-	List<SuspectAccount> sospechosos =  new ArrayList<SuspectAccount>();
+	/*
+ 	List<SuspectAccount> sospechosos =  new ArrayList<SuspectAccount>();	        
+
+		
+	 public MoneyLaunderingServiceStub (){
+	        SuspectAccount sa1 = new SuspectAccount();
+	        sa1.setAccountId("juan10");
+	        sa1.setAmountOfSmallTransactions(10000);
+	        sospechosos.add(sa1);
+	        SuspectAccount sa2 = new SuspectAccount();
+	        sa2.setAccountId("david10");
+	        sa2.setAmountOfSmallTransactions(2030);
+	        sospechosos.add(sa2);
+	        SuspectAccount sa3 = new SuspectAccount();
+	        sa3.setAccountId("navarro10");
+	        sa3.setAmountOfSmallTransactions(2144);
+	        sospechosos.add(sa3);
+	        SuspectAccount sa4 = new SuspectAccount();
+	        sa4.setAccountId("jimenez10");
+	        sa4.setAmountOfSmallTransactions(32140);
+	        sospechosos.add(sa4);
+
+	    }
+	*/
 	
     @Override
     public void updateAccountStatus(SuspectAccount suspectAccount) {
